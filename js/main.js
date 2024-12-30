1dd13ac5b6717f5f5146a0587203d47971f9a372
owen <33616236+uragunz16@users.noreply.github.com>
2024-12-30 11:52:42 +0800

    Update main.js
    removing spaces

--- js/main.js
+++ js/main.js
@@ -6,36 +6,36 @@ let container = document.createElement('div');// create div element
 container.classList.add("container");// add class name into created div 
 document.body.appendChild(container);// append container into body
 
-  // create header section with classtlist header
-  let header = document.createElement("header");// create header
-  header.classList.add("main_header");// add class in header
-  container.appendChild(header);// append header div into container
+// create header section with classtlist header
+let header = document.createElement("header");// create header
+header.classList.add("main_header");// add class in header
+container.appendChild(header);// append header div into container
 
-  let img = document.createElement("img");//create element image
-  img.classList.add("logo");// add class name in element img
-  img.setAttribute("src", "image/logo.jpg");// link file folder of img
-  img.setAttribute("width", "150");// set width for img
-  header.appendChild(img);// appendChild img inside header tag
+let img = document.createElement("img");//create element image
+img.classList.add("logo");// add class name in element img
+img.setAttribute("src", "image/logo.jpg");// link file folder of img
+img.setAttribute("width", "150");// set width for img
+header.appendChild(img);// appendChild img inside header tag
 
-  //create another div with classlist navHeader
-  let nav = document.createElement("nav");//create element div nav
-  nav.classList.add("nav_box");//add class name for nav
-  header.appendChild(nav);//appendChild inside header called nav
+//create another div with classlist navHeader
+let nav = document.createElement("nav");//create element div nav
+nav.classList.add("nav_box");//add class name for nav
+header.appendChild(nav);//appendChild inside header called nav
 
-  //loop to array and append to ul
-  let linkToNav = ["About Me", "My Portfolio", "Contact Me"]; 
-  //create ul
-  let ul = document.createElement("ul");
-    ul.classList.add("nav_ul");
-    nav.appendChild(ul);
+//loop to array and append to ul
+let linkToNav = ["About Me", "My Portfolio", "Contact Me"]; 
+//create ul
+let ul = document.createElement("ul");
+ul.classList.add("nav_ul");
+nav.appendChild(ul);
 
 //     linkToNav.forEach(a => nav.innerHTML += `<a href="#">${a}</a>`);
-  for (let i = 0; i <= linkToNav.length -1; i++) {
-    li = document.createElement("li");
-    li.classList.add("nav_items");  
-    li.innerHTML = `<a href="#">${linkToNav[i]}</a>`;
-    ul.append(li);
-  };
+for (let i = 0; i <= linkToNav.length -1; i++) {
+  li = document.createElement("li");
+  li.classList.add("nav_items");  
+  li.innerHTML = `<a href="#">${linkToNav[i]}</a>`;
+  ul.append(li);
+};
   
 // creating first section element
 let section = document.createElement("section");
